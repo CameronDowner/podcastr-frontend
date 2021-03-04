3c1ce2f86ec1fe3aea28290ea8169025f4c69776
Cameron Downer <camerondowner@rentalcars.com>
2021-03-04 22:21:18 +0000

    Add basic audio interface

--- src/pages/Genres.js
+++ src/pages/Genres.js
@@ -1,41 +1,136 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 
 import Layout from "./Layout";
-import { Card, CardContent, CardMedia, Grid, Typography } from "@material-ui/core";
+import { Card, CardContent, Grid, IconButton, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { PlayArrow } from "@material-ui/icons";
+import { FavoriteBorder, Pause, PlayArrow, SkipNext } from "@material-ui/icons";
 
 const useStyles = makeStyles({
     contentCard: {
         // backgroundColor: "transparent"
         maxWidth: "400px",
         width: "400px"
+    },
+    baseButton: {
+        textTransform: "none",
+        fontSize: "1.15rem",
+        color: "rgba(0, 0, 0, 0.87)"
+    },
+    largeIcon: {
+        "& svg": {
+            fontSize: 64
+        }
     }
 });
+
+function useRecommendedPodcast() {
+    const [loading, setLoading] = useState(false);
+    const [podcast, setPodcast] = useState({});
+
+
+    const loadPodcastRef = useRef(() => {
+        setLoading(true);
+        fetch("/api/recommend_podcast")
+          .then(res => res.json())
+          .then(setPodcast)
+          .finally(() => setLoading(false));
+    });
+
+    return { podcast, loading, loadPodcast: loadPodcastRef.current };
+}
+
+function useAudio(audio, startTimeSec = 0) {
+    const audioRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    function play() {
+        setIsPlaying(true);
+        audioRef.current.play();
+    }
+
+    function pause() {
+        setIsPlaying(false);
+        audioRef.current.pause();
+    }
+
+    function currentTime() {
+        return audioRef.current.currentTime;
+    }
+
+    useEffect(() => {
+        if (isPlaying) {
+            audioRef.current.play()
+        }
+    }, [audio]);
+
+    const AudioComponent = useMemo(() => () => (
+      <audio ref={audioRef} src={`${ audio }#t=${ startTimeSec }`} />
+    ), [audio]);
+
+    return {
+        play,
+        pause,
+        isPlaying,
+        AudioComponent
+    }
+}
 
 
 export default function Genres() {
     const classes = useStyles();
+    const { podcast, loading, loadPodcast } = useRecommendedPodcast();
+
+    const { audio, previewStartingTimeSec } = (podcast.previewEpisode || {})
+
+    const { AudioComponent, play, pause, isPlaying } = useAudio(audio, previewStartingTimeSec);
+
+    useEffect(() => loadPodcast(), []);
+
+    function handleNextButtonClick() {
+        loadPodcast();
+    }
 
     return (
       <Layout>
-          <Grid container spacing={ 4 } justify="center">
-              <Grid item className={ classes.contentCard }>
-                  <Card variant="outlined" square className={ classes.contentCard }>
-                      <CardMedia
-                        component="img"
-                        height="400"
-                        image="https://cdn-images-1.listennotes.com/podcasts/the-rough-cut-AzKVtPeMOL4-53MLh7NpAwm.300x300.jpg"
-                      />
+          <AudioComponent />
+          <Grid container spacing={ 4 }>
+              <Grid item xs={ 12 }>
+                  <Grid container spacing={ 4 } wrap="nowrap">
+                      <Grid item>
+                          <Paper style={ { width: "fit-content", height: "300px", overflow: "hidden" } }>
+                              <img
+                                src={ podcast.thumbnail } />
+                          </Paper>
+                      </Grid>
+                      <Grid item style={ { flexGrow: 1, alignSelf: "stretch" } }>
+                          <Grid container style={ { height: "100%" } } justify="center" alignContent="center">
+                              <Grid item>
+                                  <IconButton className={ classes.largeIcon }>
+                                      <FavoriteBorder />
+                                  </IconButton>
+                              </Grid>
+                              <Grid item>
+                                  <IconButton className={ classes.largeIcon }>
+                                      { !isPlaying && <PlayArrow onClick={() => play()}/> }
+                                      { isPlaying && <Pause onClick={() => pause()}/> }
+                                  </IconButton>
+                              </Grid>
+                              <Grid item>
+                                  <IconButton className={ classes.largeIcon } onClick={handleNextButtonClick}>
+                                      <SkipNext />
+                                  </IconButton>
+                              </Grid>
+                          </Grid>
+                      </Grid>
+                  </Grid>
+              </Grid>
+              <Grid item xs={ 12 }>
+                  <Card variant="outlined">
                       <CardContent>
-                          <Typography variant="h6">My Star Wars Story</Typography>
+                          <Typography variant="h6">{ podcast.title }</Typography>
                           <Typography variant="body1">
-                              My Star Wars Story is a project to document the impact of Star Wars on the lives of those
-                              in the Star Wars Generation. We're looking to tell as many people's stories as possible.
-                              If you'd like to tell your Star Wars Story, please send an email to
-                              story@mystarwarsstory.com
+                              { podcast.description }
                           </Typography>
-                          <PlayArrow fontSize="large" />
                       </CardContent>
                   </Card>
               </Grid>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,6 +4,9 @@ module.exports = function (app) {
     app.use(createProxyMiddleware("/oauth2",
       { target: "http://localhost:8080/" }
     ));
+    app.use(createProxyMiddleware("/login",
+      { target: "http://localhost:8080/" }
+    ));
     app.use(createProxyMiddleware("/api",
       { target: "http://localhost:8080/" }
     ));
